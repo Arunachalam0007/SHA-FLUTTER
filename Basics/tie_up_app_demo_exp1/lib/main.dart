7c98a3ff5034adf2110179fb21f2e7c1d5f165da
ArunSha <arunsha0007@gmail.com>
2021-02-02 14:46:15 +0530

    Network Image widget

--- Basics/tie_up_app_demo_exp1/lib/main.dart
+++ Basics/tie_up_app_demo_exp1/lib/main.dart
@@ -12,6 +12,12 @@ void main() {
           ),
           backgroundColor: Colors.green[800],
         ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://assets.materialup.com/uploads/a1fdf41e-04ab-42a2-ab43-1b559be9743b/preview.jpg'),
+          ),
+        ),
       ),
     ),
   );
