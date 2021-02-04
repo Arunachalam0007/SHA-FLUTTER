e6a0d05c398db81f65077b11f23f7b48ff85dda6
ArunSha <arunsha0007@gmail.com>
2021-02-04 17:23:23 +0530

    MI-card: Container and Safe Area Widget

--- Basics/mi_card_flutter-master/lib/main.dart
+++ Basics/mi_card_flutter-master/lib/main.dart
@@ -12,7 +12,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.lightGreenAccent,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            child: Text("MI CARD"),
+            color: Colors.white,
+            height: 100,
+            width: 100,
+            margin: EdgeInsets.only(left: 150, top: 150),
+            padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.blue,
           child: Icon(Icons.add),
