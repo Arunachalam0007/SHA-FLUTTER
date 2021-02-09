409d28a9667a6700d2fa1578abb104d690c9960f
ArunSha <arunsha0007@gmail.com>
2021-02-09 21:10:28 +0530

    MI Card: Container with Childeren

--- Basics/mi_card_flutter-master/lib/main.dart
+++ Basics/mi_card_flutter-master/lib/main.dart
@@ -13,13 +13,34 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.lightGreenAccent,
         body: SafeArea(
-          child: Container(
-            child: Text("MI CARD"),
-            color: Colors.white,
-            height: 100,
-            width: 100,
-            margin: EdgeInsets.only(left: 150, top: 150),
-            padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                child: Text("MI CARD 1"),
+                color: Colors.white,
+                height: 100.0,
+                width: 100.0,
+                // margin: EdgeInsets.only(left: 150, top: 150),
+                // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+              ),
+              Container(
+                child: Text("MI CARD 2"),
+                color: Colors.red,
+                height: 100,
+                width: 100,
+                // margin: EdgeInsets.only(left: 150, top: 150),
+                // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+              ),
+              Container(
+                child: Text("MI CARD 3"),
+                color: Colors.blue,
+                height: 100,
+                width: 100,
+                // margin: EdgeInsets.only(left: 150, top: 150),
+                // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+              ),
+            ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
