8af8c7cb1d4c78b2f4d2c1f2b00accfe1d5c04dd
ArunSha <arunsha0007@gmail.com>
2021-02-15 19:19:32 +0530

    MI-Card 1: Implemented Row and Column layout widget

--- Basics/mi_card_flutter-master/lib/main.dart
+++ Basics/mi_card_flutter-master/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,16 +12,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightGreenAccent,
+        backgroundColor:   Colors.lightGreenAccent,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+           // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+           // verticalDirection: VerticalDirection.up, // reverse order of an container and move all conter to down
+           // mainAxisAlignment: MainAxisAlignment.end, // moving all he container to the end
+           // mainAxisAlignment: MainAxisAlignment.spaceEvenly, // This will give space for front and back
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,  // This will give space only between the container or widget
+           //  crossAxisAlignment: CrossAxisAlignment.end, // this is will move end to cross(opposite)
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Container(
                 child: Text("MI CARD 1"),
                 color: Colors.white,
                 height: 100.0,
-                width: 100.0,
+              //  width: double.infinity,
                 // margin: EdgeInsets.only(left: 150, top: 150),
                 // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
               ),
@@ -40,6 +47,37 @@ class MyApp extends StatelessWidget {
                 // margin: EdgeInsets.only(left: 150, top: 150),
                 // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
               ),
+              Container(
+                child: Row(
+                  children: [ Container(
+                    child: Text("MI CARD 1"),
+                    color: Colors.white,
+                    height: 100.0,
+                    width: 100,
+                    //  width: double.infinity,
+                    // margin: EdgeInsets.only(left: 150, top: 150),
+                    // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                  ),
+                    Container(
+                      child: Text("MI CARD 2"),
+                      color: Colors.red,
+                      height: 100,
+                      width: 100,
+                      // margin: EdgeInsets.only(left: 150, top: 150),
+                      // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                    ),
+                    Container(
+                      child: Text("MI CARD 3"),
+                      color: Colors.blue,
+                      height: 100,
+                      width: 100,
+                      // margin: EdgeInsets.only(left: 150, top: 150),
+                      // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                    ),
+
+                  ],
+                ),
+              )
             ],
           ),
         ),
