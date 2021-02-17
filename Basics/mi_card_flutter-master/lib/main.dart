26fa883e57c3d41a4e72668b5805dfe7d0b52cd5
ArunSha <arunsha0007@gmail.com>
2021-02-17 13:20:15 +0530

    MI Card 2: 1. Circle Avatar widget 2. Adding Custom fonts & insert into pubspec

--- Basics/mi_card_flutter-master/lib/main.dart
+++ Basics/mi_card_flutter-master/lib/main.dart
@@ -14,108 +14,35 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 100.0,
-                color: Colors.red,
+              CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 50.0,
+                backgroundImage: AssetImage('images/arun.jpg'),
               ),
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      color: Colors.yellow,
-                      width: 100.0,
-                      height: 100.0,
-                    ),
-                    Container(
-                      color: Colors.green,
-                      width: 100.0,
-                      height: 100.0,
-                    )
-                  ],
+              Text(
+                "Arun Sha",
+                style: TextStyle(
+                  fontSize: 50.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
                 ),
               ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.blue,
-              )
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
             ],
           ),
-          // child: Column(
-          //  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //  // verticalDirection: VerticalDirection.up, // reverse order of an container and move all conter to down
-          //  // mainAxisAlignment: MainAxisAlignment.end, // moving all he container to the end
-          //  // mainAxisAlignment: MainAxisAlignment.spaceEvenly, // This will give space for front and back
-          //   // mainAxisAlignment: MainAxisAlignment.spaceBetween,  // This will give space only between the container or widget
-          //  //  crossAxisAlignment: CrossAxisAlignment.end, // this is will move end to cross(opposite)
-          //   crossAxisAlignment: CrossAxisAlignment.stretch,
-          //   children: <Widget>[
-          //     Container(
-          //       child: Text("MI CARD 1"),
-          //       color: Colors.white,
-          //       height: 100.0,
-          //     //  width: double.infinity,
-          //       // margin: EdgeInsets.only(left: 150, top: 150),
-          //       // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-          //     ),
-          //     Container(
-          //       child: Text("MI CARD 2"),
-          //       color: Colors.red,
-          //       height: 100,
-          //       width: 100,
-          //       // margin: EdgeInsets.only(left: 150, top: 150),
-          //       // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-          //     ),
-          //     Container(
-          //       child: Text("MI CARD 3"),
-          //       color: Colors.blue,
-          //       height: 100,
-          //       width: 100,
-          //       // margin: EdgeInsets.only(left: 150, top: 150),
-          //       // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-          //     ),
-          //     SizedBox(
-          //       height: 40.0,
-          //       child: Center(child: Text("Sized Box")),
-          //     ),
-          //     Container(
-          //       child: Row(
-          //         children: [ Container(
-          //           child: Text("MI CARD 1"),
-          //           color: Colors.white,
-          //           height: 100.0,
-          //           width: 100,
-          //           //  width: double.infinity,
-          //           // margin: EdgeInsets.only(left: 150, top: 150),
-          //           // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-          //         ),
-          //           Container(
-          //             child: Text("MI CARD 2"),
-          //             color: Colors.red,
-          //             height: 100,
-          //             width: 100,
-          //             // margin: EdgeInsets.only(left: 150, top: 150),
-          //             // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-          //           ),
-          //           Container(
-          //             child: Text("MI CARD 3"),
-          //             color: Colors.blue,
-          //             height: 100,
-          //             width: 100,
-          //             // margin: EdgeInsets.only(left: 150, top: 150),
-          //             // padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-          //           ),
-          //
-          //         ],
-          //       ),
-          //     )
-          //   ],
-          // ),
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.blue,
