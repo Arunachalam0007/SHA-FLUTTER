d6e725caab40f3866e86e35b220d1fe9ba833320
ArunSha <arunsha0007@gmail.com>
2021-02-23 18:03:18 +0530

    BMI: ThemeData widget for Material App

--- Basics/bmi_calculator_flutter/lib/main.dart
+++ Basics/bmi_calculator_flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +11,16 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: InputPage(),
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0D21),
+        scaffoldBackgroundColor: Color(0xFF0A0D22),
+        accentColor: Colors.purple,
+        textTheme: TextTheme(
+          body1: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 }
@@ -23,8 +35,10 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'BMI CALCULATOR',
+        title: Center(
+          child: Text(
+            'BMI CALCULATOR',
+          ),
         ),
       ),
       body: Center(
