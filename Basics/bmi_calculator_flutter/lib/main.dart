b6be11bc48defd3f113fbd2702920dc97c5cee44
ArunSha <arunsha0007@gmail.com>
2021-02-23 17:38:57 +0530

    BMI Calculator App: Created App with sample scaffold

---
+++ Basics/bmi_calculator_flutter/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BMI CALCULATOR',
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'BMI',
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
