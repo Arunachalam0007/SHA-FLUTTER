844f332dd9d460833dba14f454299087ab9e0b52
ArunSha <arunsha0007@gmail.com>
2021-03-01 18:06:28 +0530

    Clima: Geolocator package for getCurrentPosition.

--- Basics/clima_flutter_app/lib/screens/loading_screen.dart
+++ Basics/clima_flutter_app/lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,6 +7,14 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getLocation() async{
+    print('sha app Launced');
+     Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+     print('Current position $position');
+    print('sha app Closed');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,8 +22,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
-          child: Text('Get Location'),
+          child: Text('Get Locations'),
         ),
       ),
     );
