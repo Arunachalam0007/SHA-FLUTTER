4c9f6fd42622fdd58d88fee947f3956acc38296c
ArunSha <arunsha0007@gmail.com>
2021-03-02 19:58:52 +0530

    Clima Updated Project

--- Basics/clima_flutter_app/lib/screens/loading_screen.dart
+++ Basics/clima_flutter_app/lib/screens/loading_screen.dart
@@ -1,20 +1,16 @@
-import 'package:clima_flutter_app/screens/location_screen.dart';
-import 'package:clima_flutter_app/services/location.dart';
-import 'package:clima_flutter_app/utilities/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:clima_flutter_app/services/networking.dart';
+import 'location_screen.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima_flutter_app/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
-  _LoadingScreenState createState() => _LoadingScreenState();
+  State<StatefulWidget> createState() {
+    return _LoadingScreenState();
+  }
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  double longitude;
-  double latitude;
-  String networkURL;
-
   @override
   void initState() {
     super.initState();
@@ -22,15 +18,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    Duration duration = Duration(minutes: 5);
-    await location.getCurrentLocation();
-    longitude = location.longitude;
-    latitude = location.latitude;
-    networkURL =
-        'http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$kAPIKey&units=metric';
-    NetworkHelper networkHelper = NetworkHelper(url: networkURL);
-    var weatherData = await networkHelper.getResponseData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return LocationScreen(
@@ -43,9 +31,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SpinKitRotatingPlain(
-          size: 30,
-          color: Colors.red,
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
         ),
       ),
     );
--- Basics/clima_flutter_app/lib/services/networking.dart
+++ Basics/clima_flutter_app/lib/services/networking.dart
@@ -1,20 +1,20 @@
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class NetworkHelper{
-  final String url;
-  NetworkHelper({@required this.url});
+class NetworkHelper {
+  NetworkHelper(this.url);
 
-  Future getResponseData() async{
-    http.Response response = await http.get(this.url);
-    if(response.statusCode == 200){
-      String responseBody = response.body;
-      return jsonDecode(responseBody);
+  final String url;
+
+  Future getData() async {
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      return jsonDecode(data);
     } else {
-      print(response.statusCode);
       print(response.statusCode);
     }
   }
-
 }
