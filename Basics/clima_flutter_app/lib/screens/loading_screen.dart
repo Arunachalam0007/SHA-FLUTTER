f6bc2cd7f2e0792fb437a13b797f39d75c1a6298
ArunSha <arunsha0007@gmail.com>
2021-03-02 15:27:39 +0530

    Clima: using  Http package to get api body data

--- Basics/clima_flutter_app/lib/screens/loading_screen.dart
+++ Basics/clima_flutter_app/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima_flutter_app/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,18 +8,24 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   @override
   void initState() {
     super.initState();
     getLocation();
   }
 
-  void getLocation() async{
+  void getLocation() async {
     Location location = Location();
-   await location.getCurrentLocation();
+    await location.getCurrentLocation();
+    await getData();
     print(location.longitude);
     print(location.latitude);
+  }
+
+  Future<void> getData() async {
+    http.Response response = await http.get(
+        'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=3cc913675a5741818d6feaf11955e809');
+    print(response.body);
   }
 
   @override
